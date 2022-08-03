e3b8a8835d6ff99f0c7385d67cbbaee86b51c4e5
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-03 18:50:33 -0300

    hacemos que el sitio sea responsive

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -7,13 +7,13 @@ const Frase = () => {
     <div className="contenedor py-5">
       <article className="bg-fondo rounded">
         <div className="row py-3 ">
-          <div className="col-4 text-center">
+          <div className="col-12 col-md-4 text-center">
             <Card.Img className="width" src={homero} />
           </div>
-          <div className="col-8 ">
+          <div className="col-12 col-md-8 ">
             <article className="container">
               <h2 className="py-2 text-style">Homer Simpson</h2>
-              <p>
+              <p className="pl-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
                 nisi illo ea doloremque nulla asperiores odit omnis neque
                 recusandae laudantium.
