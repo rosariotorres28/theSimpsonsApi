29f2167f235ffe191bf6d6de5573bbbc10a000af
Rosarito <mariadelrosariotorres21@gmail.com>
2023-05-31 20:59:55 -0300

    agregamos el Readme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Frase from "./components/Frase";
 import "bootstrap/dist/css/bootstrap.min.css";
 import thesimpsons from "../src/components/img/theSimpson.png";
 import "../src/App.css";
 import { Button } from "react-bootstrap";
+import Spinner from "./components/Spinner"
 
 const App = () => {
+
+const [personaje, setPersonaje] = useState ({});
+const [mostrarSpinner, setMostrarSpinner] = useState (true);
+
+useEffect(()=> {
+  consultarApi()
+}, [])
+
+const consultarApi=async()=>{
+  const respuesta = await fetch ("http://thesimpsonsquoteapi.glitch.me/quotes")
+  const dato = await respuesta.json()
+  setPersonaje(dato[0])
+  setMostrarSpinner(false)
+}
+
+//operador ternario (condicion logica)? que hacer si es verdadero: que hacer si es falso
+const mostrarComponente = (mostrarSpinner===true)? <Spinner></Spinner> : <Frase personaje={personaje} ></Frase>
   return (
     <div className="bg-fondo-web">
       <div className="container py-5 text-center">
         <img src={thesimpsons} className="width" alt="thesimpsons" />
       </div>
       <div className="text-center pb-10">
-        <Button className="btn btn-color">
+        <Button className="btn btn-color" onClick={consultarApi}>
           <p className="my-0 text-btn-style">Obtener frase</p>
         </Button>
       </div>
-      <Frase></Frase>
+      
+      {mostrarComponente}
     </div>
   );
 };
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,22 +1,19 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import homero from "../components/img/Homer_Simpson.webp";
 import "../components/frase.css";
-const Frase = () => {
+const Frase = (props) => {
   return (
     <div className="contenedor py-5">
       <article className="bg-fondo rounded">
         <div className="row py-3 ">
           <div className="col-12 col-md-4 text-center">
-            <Card.Img className="width" src={homero} />
+            <Card.Img className="width" src={props.personaje.image} />
           </div>
           <div className="col-12 col-md-8 ">
             <article className="container">
-              <h2 className="py-2 text-style">Homer Simpson</h2>
+              <h2 className="py-2 text-style">{props.personaje.character}</h2>
               <p className="pl-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                nisi illo ea doloremque nulla asperiores odit omnis neque
-                recusandae laudantium.
+                {props.personaje.quote}
               </p>
             </article>
           </div>
